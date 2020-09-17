dcaf54207c24737ba05be6bc6e25be55767be155
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-09-17 22:53:10 +0300

    Added map screen

---
+++ app/screens/aviation-map-screen/aviation-map-screen.tsx
@@ -0,0 +1,66 @@
+import React from "react"
+import { observer } from "mobx-react-lite"
+import { Image, ImageStyle, Platform, TextStyle, View, ViewStyle } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import MapView from "react-native-maps"
+import { color, spacing } from "../../theme"
+import { Screen, Header, Text, Wallpaper } from "../../components"
+// import { useNavigation } from "@react-navigation/native"
+// import { useStores } from "../../models"
+
+const ROOT: ViewStyle = {
+  backgroundColor: color.palette.black,
+  flex: 1,
+}
+
+const MAP: ViewStyle = {
+  width: "100%",
+  height: "100%",
+}
+
+const BOLD: TextStyle = { fontWeight: "bold" }
+const HEADER: TextStyle = {
+  paddingTop: spacing[3],
+  paddingBottom: spacing[5] - 1,
+  paddingHorizontal: 0,
+}
+const HEADER_TITLE: TextStyle = {
+  ...BOLD,
+  fontSize: 12,
+  lineHeight: 15,
+  textAlign: "center",
+  letterSpacing: 1.5,
+}
+
+export const AviationMapScreen = observer(function AviationMapScreen() {
+  const navigation = useNavigation()
+  const goBack = () => navigation.goBack()
+  // Pull in one of our MST stores
+  // const { someStore, anotherStore } = useStores()
+  // OR
+  // const rootStore = useStores()
+
+  // Pull in navigation via hook
+  // const navigation = useNavigation()
+  return (
+    <Screen style={ROOT} preset="scroll">
+      <Wallpaper />
+      <Header
+        headerTx="demoScreen.howTo"
+        leftIcon="back"
+        onLeftPress={goBack}
+        style={HEADER}
+        titleStyle={HEADER_TITLE}
+      />
+      <MapView
+        style={MAP}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </Screen>
+  )
+})
